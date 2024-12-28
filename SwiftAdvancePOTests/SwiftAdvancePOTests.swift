526f4a20d255f0efccc56386f802f9cfe0c9bb92
Mohammad <ma@bornak.de>
2024-12-28 23:22:08 +0330

    Initial Commit

---
+++ SwiftAdvancePOTests/SwiftAdvancePOTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAdvancePOTests.swift
+//  SwiftAdvancePOTests
+//
+//  Created by Mohammad on 12/28/24.
+//
+
+import Testing
+@testable import SwiftAdvancePO
+
+struct SwiftAdvancePOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
